b375e97263eb816555e8ff508a015a09d9cf9607
Maksim Tsatsura <maxim.tsatsura@gmail.com>
2021-09-11 14:35:30 +0300

    feat: add btn catalog and search input

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
+import { SearchComponent } from './components/search/search.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [HeaderComponent],
-  imports: [CommonModule],
-  exports: [HeaderComponent],
+  declarations: [HeaderComponent, SearchComponent],
+  imports: [CommonModule, SharedModule],
+  exports: [HeaderComponent, SearchComponent],
 })
 export class CoreModule {}
